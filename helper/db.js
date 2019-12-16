3aab4493157557e27193c093e8581bc5d028f2c7
Erhan Gulmez <erhan.gulmezz@gmail.com>
2019-12-16 21:02:22 +0300

    Express generator ile npm paketler oluşturuldu ve Mongo db connection yapıldı

---
+++ helper/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+module.exports = () => {
+  mongoose.connect(
+    "mongodb+srv://erhan:erhan123@node-api-jslbx.mongodb.net/node-api?retryWrites=true&w=majority",
+    { useMongoClient: true }
+  );
+  mongoose.connection.on("open", () => {
+    console.log("DB Connected");
+  });
+  mongoose.connection.on("error", (err) => {
+    console.log("Connection Error : ",err);
+  });
+};
