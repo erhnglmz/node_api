6562f152de94dad88944f25324d4ea80ab6a55c6
Erhan Gulmez <erhan.gulmezz@gmail.com>
2019-12-20 23:10:01 +0300

    Movie apileri oluşturuldu

--- routes/movies.js
+++ routes/movies.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const ModelMovie = require("../models/Movie.js");
 
 /* GET movie listing. */
-router.get("/", (req, res, next) => {
+router.get("/", (req, res) => {
   ModelMovie.find({})
     .then((data) => {
       res.json(data);
@@ -14,13 +14,71 @@ router.get("/", (req, res, next) => {
     });
 });
 
+/* GET movie by id listing. */
+router.get("/getById", (req, res, next) => {
+  const movieId = ModelMovie.findById(req.body._id);
+  movieId
+    .then((movie) => {
+      if (!movie) next({ message: "Filim bulunamadı.", code: 404 });
+
+      res.json(movie);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+/* GET movie by id listing. */
+router.get("/:_id", (req, res, next) => {
+  const movieId = ModelMovie.findById(req.params._id);
+  movieId
+    .then((movie) => {
+      if (!movie) next({ message: "Filim bulunamadı.", code: 404 });
+
+      res.json(movie);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 /* POST movie listing. */
-router.post("/", (req, res, next) => {
+router.post("/", (req, res) => {
   const modelMovie = new ModelMovie(req.body);
   modelMovie
     .save()
     .then((data) => {
       res.json({ status: 200, data });
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+/* PUT movie listing. */
+router.put("/:_id", (req, res, next) => {
+  const movieId = ModelMovie.findByIdAndUpdate(req.params._id, req.body, {
+    new: true
+  });
+  movieId
+    .then((movie) => {
+      if (!movie) next({ message: "Filim bulunamadı.", code: 404 });
+
+      res.json({ status: 200, data: movie });
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+/* PUT movie listing. */
+router.delete("/:_id", (req, res, next) => {
+  const movieId = ModelMovie.findByIdAndRemove(req.params._id);
+  movieId
+    .then((movie) => {
+      if (!movie) next({ message: "Filim bulunamadı.", code: 404 });
+
+      res.json({ status: 200 });
     })
     .catch((err) => {
       res.json(err);
