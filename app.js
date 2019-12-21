89786c428d4d5dee31b020a286ae174ef5102863
Erhan Gulmez <erhan.gulmezz@gmail.com>
2019-12-21 13:27:24 +0300

    Token entegrasyonu yapıldı

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ const app = express();
 //Db Connection
 const db = require("./helper/db.js")();
 
+//JWT Secret Key
+const config = require("./config");
+app.set("api_secret_key", config.api_secret_key);
+
+//Middleware
+const verifyToken = require("./middleware/verify-token");
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
@@ -23,6 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/api", verifyToken);
 app.use("/api/movies", moviesRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,69 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+//Models
+const ModelUser = require("../models/User");
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
   res.render("index", { title: "Express" });
+});
+
+router.post("/register", (req, res, next) => {
+  const { username, password } = req.body;
+
+  bcrypt.hash(password, 10).then((hash) => {
+    const user = new ModelUser({
+      username: username,
+      password: hash
+    });
+
+    const userSave = user.save();
+    userSave
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+});
+
+router.post("/auth", (req, res) => {
+  const { username, password } = req.body;
+
+  ModelUser.findOne(
+    {
+      username
+    },
+    (err, user) => {
+      if (err) {
+        throw err;
+      }
+      if (!user) {
+        res.json({ status: false, message: "User not found" });
+      } else {
+        bcrypt.compare(password, user.password).then((result) => {
+          if (!result) {
+            res.json({ status: false, message: "Password is wrong" });
+          } else {
+            const payload = {
+              username
+            };
+            const token = jwt.sign(
+              payload,
+              req.app.get("api_secret_key"),
+              {
+                expiresIn: 720
+              }
+            );
+            res.json({ status: true, token });
+          }
+        });
+      }
+    }
+  );
 });
 
 module.exports = router;
